7e5967ff1ed3e31a795aa6da6bb390f1609ec56d
Joshua Mason <thelementalcode@gmail.com>
2025-04-30 14:26:50 -0700

    javascript issue with background shape event listeners

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 let isModalOpen = false;
 let contrastToggle = false;
 
+//* BACKGROUND SHAPE EVENT LISTENERS *//
+function moveBackground(event) {
+    const shapes = document.querySelectorAll('.shape')
+    const x = event.clientX;
+    const y = event.clientY;
+    
+    for (let i = 0; i < shapes.length; ++i) {
+        shapes[i].style.transform = 'translate(${x}px, ${y}px)'
+    }
+}
+
 //* TOGGLE CONTRAST - DARK MODE *//
 function toggleContrast() {
     contrastToggle = !contrastToggle;
